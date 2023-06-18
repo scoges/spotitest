c1671c4e24fa14d8c7fb8d2698f12910528bee7b
Scott Ogrins <scott.ogrins@gmail.com>
2023-06-18 17:19:31 -0300

    play

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,43 +44,7 @@ function onPageLoad(){
     }
     refreshRadioButtons();
 
-    window.onSpotifyWebPlaybackSDKReady = () => {
-      const token = TOKEN;
-      const player = new Spotify.Player({
-          name: 'Web Playback SDK Quick Start Player',
-          getOAuthToken: cb => { cb(token); },
-          volume: 0.5
-      });
-    }
-      // Ready
-      player.addListener('ready', ({ device_id }) => {
-          console.log('Ready with Device ID', device_id);
-      });
-    
-      // Not Ready
-      player.addListener('not_ready', ({ device_id }) => {
-          console.log('Device ID has gone offline', device_id);
-      });
-    
-      player.addListener('initialization_error', ({ message }) => {
-          console.error(message);
-      });
-    
-      player.addListener('authentication_error', ({ message }) => {
-          console.error(message);
-      });
-    
-      player.addListener('account_error', ({ message }) => {
-          console.error(message);
-      });
-    
-      document.getElementById('PlaySDK').onclick = function() {
-        player.PlaySDK();
-      };
-    
-      player.connect();
-    
-}
+   
 
 function handleRedirect(){
     let code = getCode();
@@ -390,4 +354,40 @@ function addRadioButton(item, index){
     node.onclick = function() { onRadioButton( item.deviceId, item.playlistId ) };
     document.getElementById("radioButtons").appendChild(node);
 }
+}
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = TOKEN;
+  const player = new Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => { cb(token); },
+      volume: 0.5
+  });
+}
+  // Ready
+  player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+  });
+
+  // Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+  });
+
+  player.addListener('initialization_error', ({ message }) => {
+      console.error(message);
+  });
+
+  player.addListener('authentication_error', ({ message }) => {
+      console.error(message);
+  });
+
+  player.addListener('account_error', ({ message }) => {
+      console.error(message);
+  });
+
+  document.getElementById('PlaySDK').onclick = function() {
+    player.PlaySDK();
+  };
+
+  player.connect();
 
