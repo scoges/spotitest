21ddc727727f67de21b7ba41753da8bf0d577f1f
Scott Ogrins <scott.ogrins@gmail.com>
2023-06-18 15:38:20 -0300

    player

--- assets/js/script.js
+++ assets/js/script.js
@@ -363,42 +363,42 @@ function addRadioButton(item, index){
     document.getElementById("radioButtons").appendChild(node);
 }
 
-//window.onSpotifyWebPlaybackSDKReady = () => {
- // const player = new Spotify.Player({
-   //   name: 'Web Playback SDK Quick Start Player',
-     // getOAuthToken: cb => { cb(TOKEN); },
-      //volume: 0.5
-    
-//  });
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = '[My access token]';
+  const player = new Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => { cb(token); },
+      volume: 0.5
+  });
 
   // Ready
- // player.addListener('ready', ({ device_id }) => {
-   //   console.log('Ready with Device ID', device_id);
-  //});
+  player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+  });
 
   // Not Ready
-  //player.addListener('not_ready', ({ device_id }) => {
-  //    console.log('Device ID has gone offline', device_id);
-  //});
+  player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+  });
 
-  //player.addListener('initialization_error', ({ message }) => {
-    //  console.error(message);
-  //});
+  player.addListener('initialization_error', ({ message }) => {
+      console.error(message);
+  });
 
-  //player.addListener('authentication_error', ({ message }) => {
-    //  console.error(message);
- // });
+  player.addListener('authentication_error', ({ message }) => {
+      console.error(message);
+  });
 
-  //player.addListener('account_error', ({ message }) => {
-   //   console.error(message);
-  //});
+  player.addListener('account_error', ({ message }) => {
+      console.error(message);
+  });
 
-  //document.getElementById('togglePlay').onclick = function() {
-  //  player.togglePlay();
-  //};
+  document.getElementById('togglePlay').onclick = function() {
+    player.togglePlay();
+  };
 
-  //player.connect();
-
+  player.connect();
+}
 
 
 
